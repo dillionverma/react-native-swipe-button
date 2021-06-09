32bbe6b7797331cf3949e3503834d01522f5d9a0
Dillion Verma <hello@dillionverma.com>
2021-06-09 19:11:37 -0400

    Update settings and bump package versions

--- example/Example.tsx
+++ example/Example.tsx
@@ -1,17 +1,17 @@
-import React, { FC } from 'react';
-import { View, Text, Alert } from 'react-native';
+import React from 'react';
+import { Alert, Text, View } from 'react-native';
 import SwipeButton from '../src/SwipeButton';
 
 const App = () => {
-    return (
-        <View style={{ flex: 1 }}>
-            <SwipeButton
-                Icon={<Text>X</Text>}
-                onComplete={() => Alert.alert('Completed')}
-                title="Swipe to complete"
-            />
-        </View>
-    );
+  return (
+    <View style={{ flex: 1 }}>
+      <SwipeButton
+        Icon={<Text>X</Text>}
+        onComplete={() => Alert.alert('Completed')}
+        title="Swipe to complete"
+      />
+    </View>
+  );
 };
 
 export default App;
--- src/SwipeButtonText.tsx
+++ src/SwipeButtonText.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import { View, Text, TextProps, StyleProp, TextStyle, ViewProps, ViewStyle, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  TextProps,
+  StyleProp,
+  TextStyle,
+  ViewProps,
+  ViewStyle,
+  StyleSheet,
+} from 'react-native';
 import { DEFAULT_HEIGHT, SwipeButtonCommonProps } from './SwipeButton';
 
-export interface SwipeButtonTextProps extends Omit<SwipeButtonCommonProps, 'borderRadius'> {
+export interface SwipeButtonTextProps
+  extends Omit<SwipeButtonCommonProps, 'borderRadius'> {
   /**
    * The text that will be displaied in the container
    */
@@ -30,42 +40,43 @@ export interface SwipeButtonTextProps extends Omit<SwipeButtonCommonProps, 'bord
 }
 
 const SwipeButtonText = ({
-    title, titleStyle, titleContainerProps, titleContainerStyle, titleProps, height = DEFAULT_HEIGHT,
+  title,
+  titleStyle,
+  titleContainerProps,
+  titleContainerStyle,
+  titleProps,
+  height = DEFAULT_HEIGHT,
 }: SwipeButtonTextProps) => {
-    return (
-        <View
-            testID="TitleContainer"
-            style={[
-                styles.titleContainer,
-                { height },
-                titleContainerStyle,
-            ]}
-            {...titleContainerProps}
-        >
-            <Text
-                numberOfLines={2}
-                allowFontScaling={false}
-                style={[styles.title, titleStyle]}
-                testID="Title"
-                {...titleProps}
-            >
-                {title}
-            </Text>
-        </View>
-    );
+  return (
+    <View
+      testID="TitleContainer"
+      style={[styles.titleContainer, { height }, titleContainerStyle]}
+      {...titleContainerProps}
+    >
+      <Text
+        numberOfLines={2}
+        allowFontScaling={false}
+        style={[styles.title, titleStyle]}
+        testID="Title"
+        {...titleProps}
+      >
+        {title}
+      </Text>
+    </View>
+  );
 };
 
 export default SwipeButtonText;
 
 const styles = StyleSheet.create({
-    titleContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    title: {
-        color: '#e9ff6b',
-        fontSize: 16,
-        maxWidth: '50%',
-        textAlign: 'center',
-    },
+  titleContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#e9ff6b',
+    fontSize: 16,
+    maxWidth: '50%',
+    textAlign: 'center',
+  },
 });
